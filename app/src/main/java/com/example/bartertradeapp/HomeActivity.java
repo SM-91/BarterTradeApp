53d44d455e5ec9be227f416b8b9aa7d7d8fb64bc
SM-91 <57176836+SM-91@users.noreply.github.com>
2020-01-04 19:34:35 +0100

    UPDATE: blah blah

--- app/src/main/java/com/example/bartertradeapp/HomeActivity.java
+++ app/src/main/java/com/example/bartertradeapp/HomeActivity.java
@@ -156,7 +156,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_signOut:
                 //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserUploadFragment()).addToBackStack(null).commit();
                 firebaseAuth.signOut();
-                //commit by talal
+                //commit by Shayan
                 break;
 
                /* case R.id.nav_signOut:
