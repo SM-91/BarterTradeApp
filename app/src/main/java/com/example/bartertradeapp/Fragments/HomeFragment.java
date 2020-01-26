d099e86b71239df6d66653446101268d39fa2715
talal438 <talalnasir2@hotmail.com>
2020-01-26 07:34:19 +0100

    Update: latest, nearest, historical Search Completed.

--- app/src/main/java/com/example/bartertradeapp/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/bartertradeapp/Fragments/HomeFragment.java
@@ -25,9 +25,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -49,6 +46,9 @@ public class HomeFragment extends BaseFragment implements custom_list_adapter.It
 
     private String ad_id,category;
 
+    //Location
+    double my_longitude=11.534293,my_latitude=49.943672;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -60,6 +60,7 @@ public class HomeFragment extends BaseFragment implements custom_list_adapter.It
         nearest_ads = new ArrayList<>();
 
         category_list = new ArrayList<>();
+
 
         // set up the RecyclerView
         recyclerView_latest = view.findViewById(R.id.recyclerView_latest);
@@ -85,38 +86,43 @@ public class HomeFragment extends BaseFragment implements custom_list_adapter.It
         adapter_nearest = new custom_nearest_adapter(getContext(), nearest_ads);
         //adapter_nearest.setClickListener(this);
 
-//
-//        // Setting data by History Data
-//        viewDatabaseReference = FirebaseDatabase.getInstance().getReference("UserHistory");
-//        viewDatabaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//
-//                category_list.clear();
-//                for (DataSnapshot usersSnapshot : dataSnapshot.getChildren()) {
-//                    for(DataSnapshot ds : usersSnapshot.getChildren()){
-//                        UserHistoryModel history = ds.getValue(UserHistoryModel.class);
-//                        //category_list.add(history.getCategory());
-//                        category=history.getCategory();
-//                    }
-//                }
-//                //category_search(category);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
+
+        // Setting data by History Data
+        viewDatabaseReference = FirebaseDatabase.getInstance().getReference("UserHistory");
+        viewDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                category_list.clear();
+                category_list.add("nothing");
+                for (DataSnapshot usersSnapshot : dataSnapshot.getChildren()) {
+                    for(DataSnapshot ds : usersSnapshot.getChildren()){
+                        UserHistoryModel history = ds.getValue(UserHistoryModel.class);
+                        //category_list.add(history.getCategory());
+                        category=history.getCategory();
+                        category_search(category);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
-        final String mytime = "26 Jan 2020 00:24:19";
-                //DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
-//        for(int i = 0; i<category_list.size() ; i++){
-//           test=category_list.get(i);
-//        }
+        // taking Latest Ads according to time...
         latest_search();
-        //nearest_search("apple");
+
+
+        // Setting data by Location
+        nearest_search();
+
+
+        //Float distance = calculateDistance(49.9342639,11.5832404,49.434669,11.051320);
+        //Toast.makeText(getContext(), "distance"+distance, Toast.LENGTH_SHORT).show();
 
         return view;
     }
@@ -128,6 +134,59 @@ public class HomeFragment extends BaseFragment implements custom_list_adapter.It
         userUploadProductModel = adapter_latest.getItem(position);
         //userUploadProductModel = adapter_nearest.getItem(position);
         //userUploadProductModel = adapter_history.getItem(position);
+        ad_id = userUploadProductModel.getAdId();
+        Date myCurrentDateTime = userUploadProductModel.getCurrentDateTime();
+        String myCurrentDateTimeString = userUploadProductModel.getCurrentDateTimeString();
+        pname = userUploadProductModel.getProductName();
+        pdesc = userUploadProductModel.getProductDescription();
+        pexch = userUploadProductModel.getPossibleExchangeWith();
+        pest = userUploadProductModel.getProductEstimatedMarketValue();
+        ptype = userUploadProductModel.getProductType();
+        pcategory = userUploadProductModel.getProductCategoryList();
+        pcondition = userUploadProductModel.getProductCondition();
+        String serviceName = userUploadProductModel.getServiceName();
+        String serviceCategory = userUploadProductModel.getServiceCategory();
+        String serviceDescription = userUploadProductModel.getServiceDescription();
+        String serviceEstimatedMarketValue = userUploadProductModel.getServiceEstimatedMarketValue();
+        String servicePossibleExchangeWith = userUploadProductModel.getServicePossibleExchangeWith();
+        String serviceImageUri = userUploadProductModel.getServiceImageUri();
+        String tag = userUploadProductModel.getTag();
+        UserModel postedBy = userUploadProductModel.getPostedBy();
+        ArrayList<String> pimagelist = userUploadProductModel.getmArrList();
+
+
+        pimg = userUploadProductModel.getmImageUri();
+
+        intent.putExtra("name", pname);
+        intent.putExtra("desc", pdesc);
+        intent.putExtra("ad_id",ad_id);
+        intent.putExtra("exchange", pexch);
+        intent.putExtra("est", pest);
+        intent.putExtra("type", ptype);
+        intent.putExtra("category", pcategory);
+        intent.putExtra("condition", pcondition);
+        intent.putExtra("exch", pexch);
+        intent.putExtra("worth", pest);
+        intent.putExtra("Key", myCurrentDateTime);
+        intent.putExtra("myCurrentDateTimeString", myCurrentDateTimeString);
+        intent.putExtra("serviceName", serviceName);
+        intent.putExtra("serviceCategory", serviceCategory);
+        intent.putExtra("serviceDescription", serviceDescription);
+        intent.putExtra("serviceEstimatedMarketValue", serviceEstimatedMarketValue);
+        intent.putExtra("servicePossibleExchangeWith", servicePossibleExchangeWith);
+        intent.putExtra("serviceImageUri", serviceImageUri);
+        intent.putExtra("tag", tag);
+        intent.putExtra("user", postedBy);
+        intent.putExtra("imagelist", pimagelist);
+        intent.putExtra("image", pimg);
+        startActivity(intent);
+    }
+    @Override
+    public void onNearestItemClick(View view, int position) {
+
+        intent = new Intent(getContext(), DetailedActivity.class);
+        //userUploadProductModel = adapter_latest.getItem(position);
+        userUploadProductModel = adapter_nearest.getItem(position);
         ad_id = userUploadProductModel.getAdId();
         Date myCurrentDateTime = userUploadProductModel.getCurrentDateTime();
         String myCurrentDateTimeString = userUploadProductModel.getCurrentDateTimeString();
@@ -189,11 +248,11 @@ public class HomeFragment extends BaseFragment implements custom_list_adapter.It
                     UserUploadProductModel latest_search = usersSnapshot.getValue(UserUploadProductModel.class);
 
                     Date datestring= latest_search.getCurrentDateTime();
-                    Toast.makeText(getContext(), ""+datestring, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(getContext(), ""+datestring, Toast.LENGTH_SHORT).show();
                     Long hour;
                     hour = printDifference(datestring, date);
-                    Toast.makeText(getContext(), ""+hour, Toast.LENGTH_SHORT).show();
-                    if (hour < 59) {
+                    Toast.makeText(getContext(), "Hour:"+hour, Toast.LENGTH_SHORT).show();
+                    if (hour < 24) {
                         latest_ads.add(latest_search);
                     }
 
@@ -232,16 +291,21 @@ public class HomeFragment extends BaseFragment implements custom_list_adapter.It
 
     }
 
-    public void nearest_search(final String nearest) {
+    public void nearest_search(){
         viewDatabaseReference = FirebaseDatabase.getInstance().getReference("ProductsAndServices");
         viewDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                nearest_ads.clear();
+
                 for (DataSnapshot usersSnapshot : dataSnapshot.getChildren()) {
-                    UserUploadProductModel latest_search = usersSnapshot.getValue(UserUploadProductModel.class);
-                    if (Pattern.compile(Pattern.quote(nearest), Pattern.CASE_INSENSITIVE).matcher(latest_search.getProductName()).find()) {
-                        nearest_ads.add(latest_search);
+                    UserUploadProductModel nearest = usersSnapshot.getValue(UserUploadProductModel.class);
+
+                    float distance;
+                    distance = calculateDistance(my_latitude,my_longitude,nearest.getLatitude(),nearest.getLongitude());
+                    Toast.makeText(getContext(), "distance"+distance, Toast.LENGTH_SHORT).show();
+                    // distance is in KM
+                    if (distance < 3) {
+                        nearest_ads.add(nearest);
                     }
                 }
                 recyclerView_nearest.setAdapter(adapter_nearest);
@@ -252,7 +316,22 @@ public class HomeFragment extends BaseFragment implements custom_list_adapter.It
 
             }
         });
+    }
 
+    public static float calculateDistance (double lat1, double lng1, double lat2, double lng2 )
+    {
+        double earthRadius = 3958.75;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double dist = earthRadius * c;
+
+        double meterConversion = 1.609;
+
+        return new Float(dist * meterConversion).floatValue();
     }
 
     public long printDifference(Date startDate, Date endDate) {
